935eb174122629aa8866cdb08b4c59952e6cd620
Rustem Mussabekov <winlinx@ya.ru>
2022-06-26 13:13:03 +0300

    Initial Commit

---
+++ cupertino/cupertinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupertinoApp.swift
+//  cupertino
+//
+//  Created by Rustem Mussabekov on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct cupertinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
