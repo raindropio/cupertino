f6e3b0bf9c400b9f2b3b278d8fc2788f529d1673
Rustem Mussabekov <winlinx@ya.ru>
2022-07-18 16:24:39 +0300

    token field ios 2

--- UI/Extensions/TokenField/TokenField.iOS.swift
+++ UI/Extensions/TokenField/TokenField.iOS.swift
@@ -14,7 +14,7 @@ struct PlatformTokenField: View {
         NavigationLink {
             TokenFieldPicker(title: title, value: $value, prompt: prompt, suggestions: suggestions, autocorrectionDisabled: autocorrectionDisabled)
         } label: {
-            Label(title, systemImage: "number")
+            Label(value.isEmpty ? title : value.joined(separator: ", "), systemImage: "number")
         }
     }
 }
@@ -26,73 +26,94 @@ struct TokenFieldPicker: View {
     var suggestions: [String]
     var autocorrectionDisabled = false
     
+    @Environment(\.dismiss) private var dismiss
     @State private var search = ""
     @FocusState private var searching: Bool
     
-    var body: some View {
-        let existing = TokenFieldUtils.filter(suggestions, value: [], by: search)
-        let unknown = TokenFieldUtils.filter(value, value: suggestions, by: search)
-        let newOne = !search.isEmpty && !(existing + unknown).contains { $0 == search }
-        
-        ZStack(alignment: .bottom) {
-            VStack(spacing: 0) {
-                Divider()
-                
-                TextField(title, text: $search, prompt: Text(prompt))
-                    .padding()
-                    .focused($searching)
-                    .submitLabel(.return)
-                    .autocorrectionDisabled(autocorrectionDisabled)
-                    .textInputAutocapitalization(autocorrectionDisabled ? .never : .sentences)
-                    .keyboardType(autocorrectionDisabled ? .webSearch : .default)
-            }
-                .textFieldStyle(.roundedBorder)
-                .controlSize(.large)
-                .zIndex(1)
-                .background(.regularMaterial)
-                .background(.systemGroupedBackground)
-            
-            List(
-                selection: .init(
-                    get: { Set(value) },
-                    set: { next in value = value.filter { next.contains($0) } + next.filter { !value.contains($0) } }
-                )
-            ) {
-                if newOne {
-                    Text("Add \(search)").tag(search)
-                }
-                
-                Section {
-                    ForEach(unknown, id:\.self) {
-                        Text($0)
-                    }
-                }
-                
-                Section {
-                    ForEach(existing, id:\.self) {
-                        Text($0)
-                    }
-                }
-                
-                Section {
-                    Spacer()
-                    Spacer()
-                }.listRowBackground(Color.clear).listRowSeparator(.hidden)
-            }
-                .environment(\.editMode, .constant(.active))
-                .animation(.easeInOut(duration: 0.25), value: value)
-                .scrollDismissesKeyboard(.never)
+    func add(_ text: String) {
+        withAnimation {
+            value = value + text
+                .split(separator: ",")
+                .map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
+                .filter { !$0.isEmpty }
         }
+    }
+    
+    var body: some View {
+        let valueFiltered = TokenFieldUtils.filter(value, value: [], by: search)
+        let suggestionsFiltered = TokenFieldUtils.filter(suggestions, value: value, by: search)
+        
+        List(
+            selection: .init(
+                get: { Set(value) },
+                set: { next in
+                    withAnimation {
+                        value = value.filter { next.contains($0) } + next.filter { !value.contains($0) }
+                    }
+                }
+            )
+        ) {
+            Section {
+                Label {
+                    HStack(spacing: 0) {
+                        TextField(title, text: $search, prompt: Text(prompt))
+                            .focused($searching)
+                            .submitLabel(.return)
+                            .autocorrectionDisabled(autocorrectionDisabled)
+                            .textInputAutocapitalization(autocorrectionDisabled ? .never : .sentences)
+                            .keyboardType(autocorrectionDisabled ? .webSearch : .default)
+                        
+                        if !search.isEmpty {
+                            Button(action: { search = "" }) {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                } icon: {
+                    Image(systemName: "plus")
+                        .foregroundColor(.secondary)
+                }
+            }
+            
+            Section {
+                ForEach(valueFiltered, id: \.self) {
+                    Text($0)
+                }
+            }
+            
+            Section {
+                ForEach(suggestionsFiltered, id:\.self) {
+                    Text($0)
+                }
+            }
+        }
+            //state & animation
+            .environment(\.editMode, .constant(.active))
+            //appearance
+            .controlSize(.large)
             .navigationTitle(value.isEmpty ? title : "Selected \(value.count)")
             .toolbar(.hidden, in: .tabBar)
+            //default focus
+            .scrollDismissesKeyboard(.never)
             .defaultFocus($searching, true)
             .task { searching = true }
+            //on comma press
+            .onChange(of: search) {
+                if $0.contains(",") {
+                    add($0)
+                }
+            }
+            //on submit press
             .onSubmit {
                 if !search.isEmpty, !value.contains(search) {
-                    value.append(search)
+                    add(search)
+                    searching = true
+                } else {
+                    dismiss()
                 }
-                searching = true
             }
+            //reset search when new item added
             .onChange(of: value) { _ in
                 if !search.isEmpty {
                     withAnimation {
