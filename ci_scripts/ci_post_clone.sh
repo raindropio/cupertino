fc088be206d3fb4c5957238ddc6b78faa6800d5f
Rustem Mussabekov <winlinx@ya.ru>
2023-05-01 15:10:22 +0300

    Fix sentry

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Install sentry
+brew install getsentry/tools/sentry-cli
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-set -e
-
-# This is necessary in order to have sentry-cli
-# install locally into the current directory
-export INSTALL_DIR=$PWD
-
-if [[ $(command -v sentry-cli) == "" ]]; then
-    echo "Installing Sentry CLI"
-    curl -sL https://sentry.io/get-cli/ | bash
-fi
-
+# Sentry upload dsym
 echo "Uploading dSYM to Sentry"
 
 sentry-cli --auth-token $SENTRY_AUTH_TOKEN \
