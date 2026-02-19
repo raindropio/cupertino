e55a305eb44b7b7c4aeabbb827098d36f8406ac6
exentrich <winlinx@yandex.com>
2026-02-19 18:29:38 +0300

    fix xcode build #4

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,11 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_UPGRADE=1
 brew install getsentry/tools/sentry-cli
 
-# Download Safari extension builds
 cd $CI_PRIMARY_REPOSITORY_PATH
+
+# Decode GoogleService-Info.plist from environment variable
+echo "$GOOGLE_SERVICE_INFO_BASE64" | base64 --decode > App/GoogleService-Info.plist
+
+# Download Safari extension builds
 sh WebExtension/download-ios.command
 sh WebExtension/download-macos.command
